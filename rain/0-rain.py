a0b7e259aa34a11ff0f61c0b3484375eeed9feb5
meekmarcelin <meekmarcelin@gmail.com>
2023-02-23 08:49:33 -0800

    1

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -2,28 +2,28 @@
 
 
 def rain(walls):
-
-    n = len(walls)
-    if n == 0:
-        return 0
-
-    left, right = 0, n-1
-    left_max, right_max = 0, 0
-    water = 0
-
-    while left < right:
-        if walls[left] < walls[right]:
-            if walls[left] >= left_max:
-                left_max = walls[left]
-            else:
-                water += left_max - walls[left]
-            left += 1
-        else:
-            if walls[right] >= right_max:
-                right_max = walls[right]
-            else:
-                water += right_max - walls[right]
-            right -= 1
-
-    return water
+  
+    # To store the maximum water
+    # that can be stored
+    res = 0
+  
+    # For every element of the array
+    for i in range(1, n - 1):
+  
+        # Find the maximum element on its left
+        left = arr[i]
+        for j in range(i):
+            left = max(left, arr[j])
+  
+        # Find the maximum element on its right
+        right = arr[i]
+  
+        for j in range(i + 1, n):
+            right = max(right, arr[j])
+  
+        # Update the maximum water
+        res = res + (min(left, right) - arr[i])
+  
+    return res
+  
 
