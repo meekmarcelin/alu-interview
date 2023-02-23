70230659cf88e2f5cd3fbca6beb8f4a41d29c438
meekmarcelin <meekmarcelin@gmail.com>
2023-02-23 09:40:45 -0800

    1

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -7,23 +7,24 @@ def rain(walls):
     if n == 0:
         return 0
 
-    left, right = 0, n-1
-    left_max, right_max = 0, 0
+    left = 0 
+    right = 0
+    left_side = 0
+    right_side = 0
     water = 0
 
     while left < right:
         if walls[left] < walls[right]:
-            if walls[left] >= left_max:
+            if walls[left] >= left_side:
                 left_max = walls[left]
             else:
-                water += left_max - walls[left]
+                water += left_side - walls[left]
             left += 1
         else:
-            if walls[right] >= right_max:
+            if walls[right] >= right_side:
                 right_max = walls[right]
             else:
                 water += right_max - walls[right]
             right -= 1
 
     return water
-
